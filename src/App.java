c3b1b0e8165f6c56c2c5f8dcf5fb283b0c1b38eb
valentinb26 <vab.wn@icloud.com>
2024-07-10 22:57:42 +0200

    major changes. Added multiple useful functions, import and export works, Creating local Data folder and removed Data folder from repo, bugfixes and comfort patches.

--- src/App.java
+++ src/App.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 public class App {
 
     public static String[] validCMD = {"HELP","USER","CREATE","DELETE","SHOW","EXIT"};
-
     public static ArrayList<String> toDoList = new ArrayList<>();
+    static boolean var = true;
+    static String SEPERATOR = "#################################################################";
+    static Scanner inputScanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
         FileIO fileIO = new FileIO();
-        Scanner inputScanner = new Scanner(System.in);
-        boolean var = true;
-        String SEPERATOR = "#################################################################";
-
-        while (var) {
         fileIO.importList();
+        
+        
+        
+        while (var) {
+
         System.out.println(SEPERATOR);
         System.out.println("Hello! What would you like to do? Type help if you need help");
         String userTask = inputScanner.nextLine().toUpperCase(); 
@@ -29,6 +31,7 @@ public class App {
                         for (int j = 0; j < validCMD.length; j++) {
                             System.out.println(validCMD[j]);
                         }
+                        continueCheck();
                         break;
                     case "USER":
                         System.out.println("WIP");
@@ -45,33 +48,75 @@ public class App {
                                 System.out.println("Ups seems like there was a problem...");
                             }
                         }
-
+                        continueCheck();
                         break;
+
                     case "DELETE":
-                        System.out.println("WIP");
+                        System.out.println("Sure! Which following task would you like to delete?");
+                        showList();
+                        String userInputDelete = inputScanner.nextLine();
+                        deleteFromList(userInputDelete);
+                        fileIO.exportList();
+                        continueCheck();
                         break;  
+
                     case "SHOW":
                         System.out.println("Sure! Let me show you your to-do's:");
-                        for (int j = 0; j < toDoList.size(); j++) {
-                            System.out.println(j+1 + ". " + toDoList.get(j));
-                        }
+                        showList();
+                        continueCheck();
                         break;   
                     case "EXIT":
                         System.out.println("Goodbye");
                         var=false;
-                    break; 
+                        break; 
                     default:
                         break;
                 }
                 break;
             }else if (i == validCMD.length-1) {
                 System.out.println("Sorry I can't help you with that.");
-                var = false;
+                continueCheck();
             } 
         }
         }
 
        
      inputScanner.close();
+     fileIO.exportList();
     }
+
+    public static void showList(){
+        for (int i = 0; i < toDoList.size(); i++) {
+            System.out.println(i+1 + ". " + toDoList.get(i));
+        }
+    }
+
+    public static void deleteFromList(String removeable){
+        for (int i = 0; i < toDoList.size(); i++) {
+            if (toDoList.get(i).equals(removeable)) {
+                toDoList.remove(i);
+                if (!toDoList.contains(removeable)) {
+                    System.out.println("Deleted  " + removeable + "  successfully");
+                }else {
+                    System.out.println("Ups seems like something went wrong while deleting...");
+                } 
+            }else if (i == toDoList.size()-1) {
+                System.out.println("Hmm can't delete what I can't find...");
+            }
+        }
+    }
+
+    public static void continueCheck(){
+        System.out.println("Would you like to continue ? Y/N");
+        String answer = inputScanner.nextLine().toUpperCase();
+        if (answer.equals("N")) {
+            System.out.println("Ok Goodbye.");
+            var = false;
+        }else if (answer.equals("Y")) {
+            var = true;
+        } else {
+            var = false;
+        }
+    }
+
 }
--- src/FileIO.java
+++ src/FileIO.java
@@ -1,29 +1,75 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FileIO {
     
-    String path = "/Users/v/Desktop/repo/toDoManger/To-do-Manager/toDoManager/Data/toDoList.txt";
+    String dataPath = "/Users/v/Desktop/repo/To-do-Manager/Data";
+    String dataName = "/toDoList.txt";
+    String dataFolder= "/toDoList";
+    String fullPath = dataPath+dataName;
+    
 
 public void importList(){
      try {
-      File myObj = new File(path);
+      File myObj = new File(dataPath+dataName);
       Scanner myReader = new Scanner(myObj);
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
-        System.out.println(data);
+        App.toDoList.add(data);
       }
       myReader.close();
     } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+      if (e instanceof FileNotFoundException) {
+        CreateFile();
+      } else {  
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
     }
-    System.out.println("imported");
 }
 
 public void exportList(){ 
-    System.out.println("exported");
+  CreateFile();
+  try {
+      FileWriter myWriter = new FileWriter(fullPath);
+
+      for (int i = 0; i < App.toDoList.size(); i++) {
+        myWriter.write(App.toDoList.get(i) + System.lineSeparator());
+      }
+      myWriter.close();
+      //System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
 }
+
+public void CreateFile() {
+  try {
+    File directory = new File(dataPath);
+    if (directory.mkdir()) {
+      System.out.println("Directory created: " + directory.getName());
+    } else {
+      //System.out.println("Directory already exists or could not be created.");
+    }
+  } catch (Exception e) {
+    System.out.println("An error occurred.");
+    e.printStackTrace();
+  }
+  try {
+    File myObj = new File(fullPath);
+    if (myObj.createNewFile()) {
+      System.out.println("File created: " + myObj.getName());
+    } else {
+      //System.out.println("File already exists.");
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
 
 }
