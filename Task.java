444429f0b006249d1ee5bed6db064c0865c0de3e
valentinb26 <vab.wn@icloud.com>
2026-01-16 14:41:38 +0100

    enhanced Task.java

--- Task.java
+++ Task.java
@@ -1,14 +1,28 @@
 public class Task {
     private String description;
     private boolean isCompleted;
+    private String owner;
 
-    public Task(String description) {
+    public Task(String description, String owner) {
         this.description = description;
+        this.owner = owner;
         this.isCompleted = false;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 
     public boolean isCompleted() {
