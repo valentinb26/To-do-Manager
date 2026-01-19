beb5b6dceaa63abebbc4a9f6e83f24df9060880f
valentinb26 <vab.wn@icloud.com>
2026-01-19 17:20:46 +0100

    added documentation, todos, ideas etc. maybe and improved the showList methode (using iterator now)

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class App {
 
     public static String[] validCMD = {"HELP","USER","CREATE","DELETE","SHOW","EXIT"};
-    //auf jedenfall String durch ganze richtige Objekte ersetzen
     public static ArrayList<Task> toDoList = new ArrayList<>();
     static String SEPERATOR = "#################################################################";
     static Scanner inputScanner = new Scanner(System.in);
@@ -41,6 +41,7 @@ public class App {
                         System.out.println("WIP");
                         break;
                     case "CREATE":
+                        //create blank task, ask for description and owner, add to list
                         Task newTask = new Task("","",false); //placeholder
                         System.out.println("Sure! Please enter the Name of your task.");
                         String userInputTaskDescription = inputScanner.nextLine();
@@ -51,6 +52,7 @@ public class App {
                         toDoList.add(newTask);
 
                         //todo: add check if sucessfull added task
+                        //todo: unique IDs for tasks
 
                         /* 
                         for (int j = 0; j < toDoList.size(); j++) {
@@ -67,48 +69,58 @@ public class App {
                         break;
 
                     case "DELETE":
+                        // show list, ask which one to delete, delete from list
                         System.out.println("Sure! Which following task would you like to delete?");
                         showList();
                         String userInputDelete = inputScanner.nextLine();
                         deleteFromList(userInputDelete);
                         fileIO.exportList();
-                        //frag ob man noch mehr löschen will
+                        //inspiration: ask if wanting to delete more
                         running = continueCheck();
                         break;  
 
                     case "SHOW":
+                        //display all tasks
                         System.out.println("Sure! Let me show you your to-do's:");
                         
                         showList();
                         running = continueCheck();
                         break;   
+                        //inspiration: sort options
                     case "EXIT":
+                        //exit program
                         System.out.println("Goodbye");
                         running = false;
-                        break; 
-                    //einen sort case oder eine sort option in show einbauen    
+                        break;     
                     default:
                         break;
                 }
                 break;
             }else if (i == validCMD.length-1) {
+                //if command not recognized and run again
                 System.out.println("Sorry I can't help you with that.");
                 running = continueCheck();
             } 
         }
         }
 
-       
+    //cleanup before exit
      inputScanner.close();
      fileIO.exportList();
     }
 
+    // method to show the to-do list
     public static void showList(){
-        for (int i = 0; i < toDoList.size(); i++) {
-            System.out.println(i+1 + ". " + toDoList.get(i));
+        Iterator<Task> iterator = toDoList.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
         }
     }
 
+    // method to delete all tasks with the given description from the to-do list
+    // might delete multiple tasks if they share the same description
+    // todo: give each task a unique ID to delete specific tasks
     public static void deleteFromList(String removeable){
         boolean removed = toDoList.removeIf(task -> task.getDescription().equals(removeable));
         if (removed) {
@@ -118,6 +130,8 @@ public class App {
         }
     }
 
+    // method to ask the user if they want to continue
+    // user will start over if they type Y, otherwise the program will exit
     public static boolean continueCheck() {
         System.out.println("Would you like to continue? Y/N");
         String answer = inputScanner.nextLine().trim().toUpperCase();
