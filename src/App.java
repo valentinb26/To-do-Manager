d13347f9b60c792a10ac6abd47b272f46d2c9409
valentinb26 <vab.wn@icloud.com>
2026-01-20 14:21:13 +0100

    split switch into more methods and refractored App to use App object with the new run methode to handle everything

--- src/App.java
+++ src/App.java
@@ -6,12 +6,16 @@ import java.util.Scanner;
 public class App {
 
     public static String[] validCMD = {"HELP","USER","CREATE","DELETE","SHOW","EXIT"};
-    public static List<Task> toDoList = new ArrayList<>();
+    public List<Task> toDoList = new ArrayList<>();
     static String SEPERATOR = "#################################################################";
     static Scanner inputScanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-
+        App app = new App();
+        app.run();
+    }
+       
+    public void run() throws Exception {
         FileIO fileIO = new FileIO();
         toDoList = fileIO.importList();
         
@@ -29,11 +33,7 @@ public class App {
 
                 switch (userTask) {
                     case "HELP":
-                        System.out.println("Sure! Let me show you your possible commands: ");
-                        for (int j = 0; j < validCMD.length; j++) {
-                            System.out.println(validCMD[j]);
-                        }
-                        running = continueCheck();
+                        helpMenu();
                         break;
                     case "USER":
                     /* 
@@ -43,56 +43,15 @@ public class App {
                         System.out.println("WIP");
                         break;
                     case "CREATE":
-                        //create blank task, ask for description and owner, add to list
-                        Task newTask = new Task("","",false); //placeholder
-                        System.out.println("Sure! Please enter the description of your task.");
-                        String userInputTaskDescription = inputScanner.nextLine();
-                        newTask.setDescription(userInputTaskDescription);
-                        System.out.println("Please enter the Owner of the task.");
-                        String userInputTaskOwner = inputScanner.nextLine();
-                        newTask.setOwner(userInputTaskOwner);
-                        toDoList.add(newTask);
-
-                        //todo: add check if sucessfull added task
-                        //todo: unique IDs for tasks
-
-                        /* 
-                        for (int j = 0; j < toDoList.size(); j++) {
-                            if (toDoList.get(j).equals(userInputTaskDescription) ) {
-                                System.out.println("Succesfully added: " + toDoList.get(j));
-                                fileIO.exportList();
-                            }else if (j == toDoList.size()-1) {
-                                System.out.println("Ups seems like there was a problem...");
-                            }
-                        }
-                            */
-                        //frag ob man noch mehr erstellen will
-                        running = continueCheck();
+                        createTask();
+                        fileIO.exportList(toDoList);
                         break;
-
                     case "DELETE":
-                        // show list, ask which one to delete, delete from list
-                        // currently selecting via description, might delete multiple if same description
-
-                        // todo: unique IDs for tasks
-                        // todo: confirm before delete
-                        // todo: ask if wanting to delete more
-                        // inspiration: delete all completed tasks option
-
-                        // displays list with numbering so maybe user could select via number 
-                        // instead of description
-                        System.out.println("Sure! Which following task would you like to delete? (select via description)");
-                        showList();
-                        String userInputDelete = inputScanner.nextLine();
-                        deleteFromList(userInputDelete);
+                        deleteTask();
                         fileIO.exportList(toDoList);
                         running = continueCheck();
                         break;  
-
                     case "SHOW":
-                        // display all tasks
-                        // todo: sort options (owner, completed, etc)
-                        System.out.println("Sure! Let me show you your to-do's:");
                         showList();
                         running = continueCheck();
                         break;   
@@ -115,14 +74,15 @@ public class App {
         }
         }
 
-    //cleanup before exit
-    inputScanner.close();
-    fileIO.exportList(toDoList);
+        //cleanup before exit
+        inputScanner.close();
+        fileIO.exportList(toDoList);
     }
 
     // method to show the to-do list using an iterator
     // order depends on when tasks were added (1. is oldest)
-    public static void showList(){
+    public void showList(){
+        System.out.println("Sure! Let me show you your to-do's:");
         Iterator<Task> iterator = toDoList.iterator();
         int i = 1;
         while (iterator.hasNext()) {
@@ -134,7 +94,7 @@ public class App {
     // method to delete all tasks with the given description from the to-do list
     // might delete multiple tasks if they share the same description
     // todo: give each task a unique ID to delete specific tasks
-    public static void deleteFromList(String removeable){
+    public void deleteFromList(String removeable){
         boolean removed = toDoList.removeIf(task -> task.getDescription().equals(removeable));
         if (removed) {
             System.out.println("Deleted  " + removeable + "  successfully");
@@ -145,11 +105,37 @@ public class App {
 
     // method to ask the user if they want to continue
     // user will start over if they type Y, otherwise the program will exit
-    public static boolean continueCheck() {
+    public boolean continueCheck() {
         System.out.println("Would you like to continue? Y/N");
         String answer = inputScanner.nextLine().trim().toUpperCase();
 
         return answer.equals("Y");
+    }
+
+    public void helpMenu() {
+        System.out.println("Sure! Let me show you your possible commands: ");
+        for (int j = 0; j < validCMD.length; j++) {
+            System.out.println(validCMD[j]);
+        }
+    }
+
+    public void createTask() {
+        System.out.println("Let's create a new task!");
+        System.out.println("Please enter the task description:");
+        String description = inputScanner.nextLine();
+
+        System.out.println("Please enter the task owner:");
+        String owner = inputScanner.nextLine();
+
+        Task newTask = new Task(description, owner, false);
+        toDoList.add(newTask);
+        System.out.println("Task created successfully: " + newTask);
+    }
+
+    public void deleteTask() {
+        System.out.println("Please enter the description of the task you want to delete:");
+        String description = inputScanner.nextLine();
+        deleteFromList(description);
     }
 
 }
